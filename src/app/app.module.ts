bc63ed154b2b8065ded893c6d995cfac19c7a543
Andrea Trajkovic <trajkovicandrea@gmail.com>
2019-06-13 22:42:27 +0200

    Revert "Initial commit"
    This reverts commit 59d054fafe1cccddf970e1aeb73af8fe0e832d97.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
-import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule,
-    AngularFireDatabaseModule
-
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
