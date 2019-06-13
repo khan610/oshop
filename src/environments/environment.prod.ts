59d054fafe1cccddf970e1aeb73af8fe0e832d97
Andrea Trajkovic <trajkovicandrea@gmail.com>
2019-06-13 22:28:15 +0200

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBsX4fNXwrNZjN_ExqbyN4bb9Ty_icuvRY",
+    authDomain: "oshop-8ae81.firebaseapp.com",
+    databaseURL: "https://oshop-8ae81.firebaseio.com",
+    projectId: "oshop-8ae81",
+    storageBucket: "oshop-8ae81.appspot.com",
+    messagingSenderId: "893539404821",
+    appId: "1:893539404821:web:5ec4803864a13418"
+  }
 };
